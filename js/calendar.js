cc20c894ce57fe6d98d3cbd676fdb468564738e3
Jean-Christophe Taveau <jeanchristophe.taveau@gmail.com>
2015-07-31 16:48:07 +0200

    Fix ISO week date

--- js/calendar.js
+++ js/calendar.js
@@ -1,5 +1,10 @@
 
+console.log(getWeekNum(2015,11,28));
+console.log(getWeekNum(2016,0,1));
+
+
 setCalendarHeader();
+
 
 function setCalendarHeader() {
     var now = new Date();
@@ -29,7 +34,7 @@ function updateCalendarHeader() {
     element.innerHTML = html;
     
     // Update Week Number
-    var weekNum = document.getElementById('weeknum').innerHTML = 'Week ' + getWeekNum(y,m,d);
+    var weekNum = document.getElementById('weeknum').innerHTML = 'Week ' + getISOWeekNum(y,m,d);
 }
 
 function getWeekDays(y,m,d) {
@@ -73,8 +78,43 @@ function nextWeek() {
 
 // From pikaday 
 // https://github.com/dbushell/Pikaday/blob/master/pikaday.js
-// HACK: Problem with year = 2015 - the last week is *53* . This formula doesn't work
+// This formula follows the US norm 
 function getWeekNum(y,m,d) {
     var firstjan = new Date(y, 0, 1);
     return Math.ceil((((new Date(y, m, d) - firstjan) / 86400000) + firstjan.getDay()+1)/7);
 }
+
+function getISOWeekNum(y,m,d) {
+/**
+ * Get the ISO week date week number
+ * From http://techblog.procurios.nl/k/n618/news/view/33796/14863/calculate-iso-8601-week-and-year-in-javascript.html
+ */
+    var date = new Date(y,m,d);
+	// Create a copy of this date object
+	var target  = new Date(y,m,d);
+
+	// ISO week date weeks start on monday
+	// so correct the day number
+	var monday   = (date.getDay() + 6) % 7;
+
+	// ISO 8601 states that week 1 is the week
+	// with the first thursday of that year.
+	// Set the target date to the thursday in the target week
+	target.setDate(target.getDate() - monday + 3);
+
+	// Store the millisecond value of the target date
+	var firstThursday = target.valueOf();
+
+	// Set the target to the first thursday of the year
+	// First set the target to january first
+	target.setMonth(0, 1);
+	// Not a thursday? Correct the date to the next thursday
+	if (target.getDay() != 4) {
+		target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
+	}
+
+	// The weeknumber is the number of weeks between the 
+	// first thursday of the year and the thursday in the target week
+	return 1 + Math.ceil((firstThursday - target) / 604800000); // 604800000 = 7 * 24 * 3600 * 1000
+}
+
