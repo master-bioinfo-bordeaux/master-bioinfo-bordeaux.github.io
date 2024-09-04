22e936e5cb270a0a6ef0712318f9d37bdeb152d6
Jean-Christophe Taveau <jeanchristophe.taveau@u-bordeaux.fr>
2024-09-04 15:00:23 +0200

    Bug Fix

--- src/calmgr.js
+++ src/calmgr.js
@@ -424,7 +424,7 @@ const calDB = {
       },
       "4TBC901U": {
         semester: "S09",
-        source: "S9_POO2",
+        source: "S9_POO",
         acronym: "S09::POO++",
         apogee: "4TBC901U",
         isCourse: true,
